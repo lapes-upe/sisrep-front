8117361001d9ced61e86b149a4f486ecc87352b8
Brenoly Porto <brenoly.porto@upe.br>
2022-06-14 19:53:20 -0300

    feature: terminando arquitetura.

--- src/layouts/HeaderPrimary/HeaderPrimary.tsx
+++ src/layouts/HeaderPrimary/HeaderPrimary.tsx
@@ -2,6 +2,7 @@ import "./HeaderPrimary.scss";
 import { StyleSheet } from "../../utils/StyleSheet";
 import ContainerCustom from "../ContainerCustom/ContainerCustom";
 import { Link } from "react-router-dom";
+import { Box, Typography } from '@mui/material';
 
 function HeaderPrimary() {
   const styles = StyleSheet.create({
@@ -11,24 +12,24 @@ function HeaderPrimary() {
   });
 
   return (
-    <header className="headerPrimary">
+    <Box component='header' className="headerPrimary">
       <ContainerCustom>
-        <div className="headerBody">
-          <div className="left">
-            <strong>SISREP</strong>
-          </div>
-          <div className="rigth">
-            <ul>
-              <li><Link to={'/'}>Início</Link></li>
-              <div className="verticalLine"></div>
-              <li><Link to={'/sobre'}>Sobre</Link></li>
-              <div className="verticalLine"></div>
-              <li><Link to={'/contato'}>Contato</Link></li>
-            </ul>
-          </div>
-        </div>
+        <Box className="headerBody">
+          <Box className="left">
+            <Typography>SISREP</Typography>
+          </Box>
+          <Box className="rigth">
+            <Box component='ul'>
+              <Box component='li'><Link to={'/'}>Início</Link></Box>
+              <Box className="verticalLine"></Box>
+              <Box component='li'><Link to={'/sobre'}>Sobre</Link></Box>
+              <Box className="verticalLine"></Box>
+              <Box component='li'><Link to={'/contato'}>Contato</Link></Box>
+            </Box>
+          </Box>
+        </Box>
       </ContainerCustom>
-    </header>
+    </Box>
   );
 }
 
